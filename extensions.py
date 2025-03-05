e67056774120f3ef8d82687dbe80f173d2f9e8cb
qczeck <maciej.kuczek@icloud.com>
2025-03-05 13:20:34 +0000

    fix: tests

--- extensions.py
+++ extensions.py
@@ -9,7 +9,7 @@ bcrypt = Bcrypt()
 
 redis_client = redis.Redis(
     host=str(os.getenv("REDIS_HOST")),
-    port=int(os.getenv("REDIS_PORT")),
+    port=int(os.getenv("REDIS_PORT", 6380)),
     password=str(os.getenv("REDIS_PASSWORD")),
     ssl=True,
     ssl_cert_reqs=None,
